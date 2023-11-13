28befe3c1d97b2fadbbcccd7ea0d3680320ce37c
plumnet <143989967+Plumnet@users.noreply.github.com>
2023-11-13 06:58:31 +0000

    スプレッド構文

--- src/index.mjs
+++ src/index.mjs
@@ -98,11 +98,54 @@
 // const message2 = `名前は${name}です。年齢は${age}歳です。`;
 // console.log(message2);
 
-const myProfile = ['じゃけぇ', 28];
+// const myProfile = ["じゃけぇ", 28];
 
-const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-console.log(message3);
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
 
-const [name, age] = myProfile;
-const message4  = `名前は${name}です。年齢は${age}歳です。`;
-console.log(message4);
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`);
+// sayHello("じゃけぇ");
+
+/**
+ * スプレッド構文...
+ */
+//配列の展開
+// const arr1 = [1, 2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+//まとめる
+const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1);
+console.log(num2);
+console.log(arr3);
+
+//配列のコピー、結合、ここはよくReactで使われるらしい
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+arr6[0] = 100;
+console.log(arr6);
+console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// cnsole.log(arr7);
+
+//別に=で直接書いてもいいのではないか、という疑問に対する
+//検証のコード、しかし直接=は問題がある
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr4);
